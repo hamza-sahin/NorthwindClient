66d1a0dcaf9291b486975d18386f51a078f6f0a4
Hamza <hsahin2@binghamton.edu>
2021-02-22 12:18:11 +0300

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -13,14 +14,22 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/orders', require('./routes/api/orders'));
 app.use('/api/products', require('./routes/api/products'));
 app.use('/api/customers', require('./routes/api/customers'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
